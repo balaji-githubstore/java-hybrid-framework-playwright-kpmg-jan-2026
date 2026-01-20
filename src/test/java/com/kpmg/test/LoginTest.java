da3bedf069e305f1a381758c768cf0a720c16cfe
balaji-githubstore <balajidinakaran1@gmail.com>
2026-01-20 10:23:56 +0530

    Work on invalidLoginTest

--- src/test/java/com/kpmg/test/LoginTest.java
+++ src/test/java/com/kpmg/test/LoginTest.java
@@ -13,9 +13,20 @@ public class LoginTest extends AutomationWrapper {
 		page.locator("xpath=//input[@name='username']").fill("Admin");
 		page.locator("xpath=//input[@name='password']").fill("admin123");
 		page.locator("xpath=//button[contains(normalize-space(),'Login')]").click();
-		//Assert the header - Dashboard
+
 		String actualValue=page.locator("xpath=//h6[contains(normalize-space(),'Dashb')]").innerText();
 		Assert.assertEquals(actualValue, "Dashboard");
+	}
+	
+	@Test
+	public void invalidLoginTest()
+	{
+		page.locator("xpath=//input[@name='username']").fill("john");
+		page.locator("xpath=//input[@name='password']").fill("john123");
+		page.locator("xpath=//button[contains(normalize-space(),'Login')]").click();
+		
+		//Assert the error - Invalid credentials
+
 	}
 
 }
