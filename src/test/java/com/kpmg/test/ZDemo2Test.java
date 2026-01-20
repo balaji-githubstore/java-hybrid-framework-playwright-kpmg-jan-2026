f95ad2364afd26139cbfa4059510f1936694ba66
balaji-githubstore <balajidinakaran1@gmail.com>
2026-01-20 13:22:41 +0530

    Code to Covert Sheet to Object[][] in main method

--- src/test/java/com/kpmg/test/ZDemo2Test.java
+++ src/test/java/com/kpmg/test/ZDemo2Test.java
@@ -3,6 +3,7 @@ package com.kpmg.test;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -13,20 +14,22 @@ public class ZDemo2Test {
 
 	public static void main(String[] args) throws IOException {
 
-		FileInputStream file = new FileInputStream("test-data/orange-hrm-data.xlsx");
+		FileInputStream file = new FileInputStream("test-data/orange-hrm-data1.xlsx");
 
 		XSSFWorkbook book = new XSSFWorkbook(file);
 
 		XSSFSheet sheet = book.getSheet("invalidLoginTest");
-		
-		Object[][] data=new Object[2][3];
 
-		// write logic to print all cell values
-		for (int r = 1; r < 3; r++) {
-			for (int c = 0; c < 3; c++) {
-				String value = sheet.getRow(r).getCell(c).getStringCellValue();
-				System.out.println(value);
-				data[r-1][c]=value;
+		int rowCount = sheet.getPhysicalNumberOfRows();
+		int cellCount = sheet.getRow(0).getPhysicalNumberOfCells();
+
+		Object[][] data = new Object[rowCount - 1][cellCount];
+
+		DataFormatter format=new DataFormatter();
+		
+		for (int r = 1; r < rowCount; r++) {
+			for (int c = 0; c < cellCount; c++) {
+				data[r - 1][c] = format.formatCellValue(sheet.getRow(r).getCell(c));
 			}
 		}
 
