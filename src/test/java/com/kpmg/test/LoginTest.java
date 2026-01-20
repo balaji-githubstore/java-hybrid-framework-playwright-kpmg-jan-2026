11a84951492d91e1916f314d75db1a0e48569a0e
balaji-githubstore <balajidinakaran1@gmail.com>
2026-01-20 11:33:12 +0530

    Complete dataprovider for invalidLoginTest

--- src/test/java/com/kpmg/test/LoginTest.java
+++ src/test/java/com/kpmg/test/LoginTest.java
@@ -6,28 +6,31 @@ import org.testng.annotations.Test;
 import com.kpmg.base.AutomationWrapper;
 
 public class LoginTest extends AutomationWrapper {
-	
+
 	@Test
-	public void validLoginTest()
-	{
+	public void validLoginTest() {
 		page.locator("xpath=//input[@name='username']").fill("Admin");
 		page.locator("xpath=//input[@name='password']").fill("admin123");
 		page.locator("xpath=//button[contains(normalize-space(),'Login')]").click();
 
-		String actualValue=page.locator("xpath=//h6[contains(normalize-space(),'Dashb')]").innerText();
+		String actualValue = page.locator("xpath=//h6[contains(normalize-space(),'Dashb')]").innerText();
 		Assert.assertEquals(actualValue, "Dashboard");
 	}
 	
-	@Test
-	public void invalidLoginTest()
-	{
-		page.locator("xpath=//input[@name='username']").fill("john");
-		page.locator("xpath=//input[@name='password']").fill("john123");
-		page.locator("xpath=//button[contains(normalize-space(),'Login')]").click();
-		
-		String actualError=page.locator("xpath=//p[contains(normalize-space(),'Invalid')]").innerText();
-		Assert.assertEquals(actualError, "Invalid credentials");
+	//create a @DataProvider - invalidLoginData()
+//	saul, saul123, Invalid credentials
+//	john, john123, Invalid credentials
+	//will resume at 11:30 AM IST
 
+	@Test
+	public void invalidLoginTest(String username, String password, String expectedError) 
+	{
+		page.locator("xpath=//input[@name='username']").fill(username);
+		page.locator("xpath=//input[@name='password']").fill(password);
+		page.locator("xpath=//button[contains(normalize-space(),'Login')]").click();
+
+		String actualError = page.locator("xpath=//p[contains(normalize-space(),'Invalid')]").innerText();
+		Assert.assertEquals(actualError, expectedError);
 	}
 
 }
