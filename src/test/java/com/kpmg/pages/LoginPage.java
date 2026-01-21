dbb210db4e00ab995c2e43dd39c09f3db40c8efa
balaji-githubstore <balajidinakaran1@gmail.com>
2026-01-21 12:13:25 +0530

    Updated Page Object

--- src/test/java/com/kpmg/pages/LoginPage.java
+++ src/test/java/com/kpmg/pages/LoginPage.java
@@ -2,32 +2,29 @@ package com.kpmg.pages;
 
 import com.microsoft.playwright.Page;
 
-public class LoginPage   {
+public class LoginPage {
 
 	private Page page;
-	public LoginPage(Page page)
-	{
-		this.page=page;
+
+	public LoginPage(Page page) {
+		this.page = page;
 	}
 
 	public void enterUsername(String username) {
-		
+
 		page.locator("xpath=//input[@name='username']").fill(username);
 	}
 
 	public void enterPassword(String password) {
 		page.locator("xpath=//input[@name='password']").fill(password);
 	}
-	
-	public void clickOnLogin()
-	{
+
+	public void clickOnLogin() {
 		page.locator("xpath=//button[contains(normalize-space(),'Login')]").click();
 	}
-	
-	public String getInvalidErrorMessage()
-	{
+
+	public String getInvalidErrorMessage() {
 		return page.locator("xpath=//p[contains(normalize-space(),'Invalid')]").innerText();
 	}
-	
 
 }
