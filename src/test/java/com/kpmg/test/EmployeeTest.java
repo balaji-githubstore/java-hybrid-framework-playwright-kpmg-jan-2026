40826971b5cef51f3a1e61f2d6a95d720f936e54
balaji-githubstore <balajidinakaran1@gmail.com>
2026-01-23 09:50:30 +0530

    Added Employee Test

---
+++ src/test/java/com/kpmg/test/EmployeeTest.java
@@ -0,0 +1,13 @@
+package com.kpmg.test;
+
+import com.kpmg.base.AutomationWrapper;
+import org.testng.annotations.Test;
+
+public class EmployeeTest extends AutomationWrapper {
+
+    @Test
+    public void addValidEmployeeTest()
+    {
+
+    }
+}
