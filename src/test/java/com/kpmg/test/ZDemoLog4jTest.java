b59b5a81b3e9af1bbd6972f18b4519261f6882b2
balaji-githubstore <balajidinakaran1@gmail.com>
2026-01-28 19:51:48 +0530

    Log4j for reference

--- src/test/java/com/kpmg/test/ZDemoLog4jTest.java
+++ src/test/java/com/kpmg/test/ZDemoLog4jTest.java
@@ -4,6 +4,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 
+/**
+ * Add mvn - log4j-api, log4j-core, log4j-slf4j2-impl from groupid- org.apache.logging.log4j
+ * Add log4j2.xml with log level as DEBUG
+ * Add below code
+ */
 public class ZDemoLog4jTest {
 	private static final Logger log =
 	        LogManager.getLogger(ZDemoLog4jTest.class);
