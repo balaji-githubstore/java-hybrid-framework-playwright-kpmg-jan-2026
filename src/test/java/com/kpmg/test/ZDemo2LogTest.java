995370614a7298c40daafe9431539416b5348752
balaji-githubstore <balajidinakaran1@gmail.com>
2026-01-29 11:19:19 +0530

    Updated code log4j

--- src/test/java/com/kpmg/test/ZDemo2LogTest.java
+++ src/test/java/com/kpmg/test/ZDemo2LogTest.java
@@ -4,7 +4,11 @@ package com.kpmg.test;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
-
+/**
+ * 1. update pom.xml for log4j libraries
+ * 2. add src/test/resources/log4j2.xml
+ * 3. try below code 
+ */
 public class ZDemo2LogTest {
 	 private static final Logger LOGGER = LogManager.getLogger(ZDemo2LogTest.class);
 	 
